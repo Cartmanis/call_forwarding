49a081125d7dc64d8c945474ff2470aba92ed9e6
cartmanis <vshmelcer@gmail.com>
2019-12-27 11:03:13 +0700

    Добавлена переадресация

--- app/main.go
+++ app/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"github.com/cartmanis/call_forwarding/app/forward"
+	"github.com/cartmanis/call_forwarding/app/models"
+	"github.com/cartmanis/call_forwarding/logger"
+)
 
+func main() {
+	c := make(chan int)
+	s := &models.Settings{
+		ListnerIP:   "172.22.2.60",
+		ListnerPort: 7371,
+		ForwardIP:   "192.168.41.26",
+		ForwardPort: 3050,
+		Comment:     "Some Comment",
+	}
+	f, err := forward.NewForward(s)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	go f.StartListner()
+	<-c
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cartmanis/call_forwarding
+
+go 1.12
